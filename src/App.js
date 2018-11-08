700c68be79d4b55b2475290ea07bcae048575f23
Danny Pham <dannyhp1@uci.edu>
2018-11-08 14:26:02 -0800

    Modified homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Navigation from './Navigation/Navigation';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello world!</p>
+        <Navigation />
       </div>
     );
   }
