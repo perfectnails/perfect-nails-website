c1f0cd1ec1e6fb2eda1a1c6d9fd9ed31202e797f
Danny Pham <dannyhp1@uci.edu>
2018-11-08 19:07:20 -0800

    Added prices page

--- src/Prices/Prices.js
+++ src/Prices/Prices.js
@@ -4,7 +4,135 @@ import './Prices.css';
 const Prices = (props) => {
     return (
         <div className='container col-12 md-top-padding'>
-            <p>Page under construction!</p>
+            <div className='col-12 sm-bottom-padding'>
+                <h1><strong>Services & Prices</strong></h1>
+            </div>
+            <div className='col-6 left-float'>
+            <table className="table-fill">
+                <thead>
+                <tr>
+                <th>Service (Manicure & Pedicure)</th>
+                <th>Price</th>
+                </tr>
+                </thead>
+                <tbody clasName="table-hover">
+                <tr>
+                    <td>Manicure & Pedicure w/ Spa</td>
+                    <td>$20.00</td>
+                </tr>
+                <tr>
+                    <td>Manicure</td>
+                    <td>$10.00</td>
+                </tr>
+                <tr>
+                    <td>Spa Pedicure</td>
+                    <td>$14.00</td>
+                </tr>
+                <tr>
+                    <td>Regular Pedicure</td>
+                    <td>$13.00</td>
+                </tr>
+                <tr>
+                    <td>French Manicure</td>
+                    <td>$4.00 EXTRA</td>
+                </tr>
+                <tr>
+                    <td>American Manicure</td>
+                    <td>$4.00 EXTRA</td>
+                </tr>
+                <tr>
+                    <td>Polish Change</td>
+                    <td>$5.00</td>
+                </tr>
+                <tr>
+                    <td>French Polish Change</td>
+                    <td>$7.00</td>
+                </tr>
+                <tr>
+                    <td>American Polish Change</td>
+                    <td>$7.00</td>
+                </tr>
+                <tr>
+                    <td>Nails Art</td>
+                    <td>$2.00 & up per nail</td>
+                </tr>
+                <tr>
+                    <td>Nail Repair</td>
+                    <td>$3.00 & up</td>
+                </tr>
+                <tr>
+                    <td>Toe Nail Repair</td>
+                    <td>$4.00</td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <br/>
+            </div>
+
+            <div className='col-6 right-float'>
+            <table className="table-fill">
+                <thead>
+                <tr>
+                <th>Service (Nails Care)</th>
+                <th>Price</th>
+                </tr>
+                </thead>
+                <tbody clasName="table-hover">
+                <tr>
+                    <td>Full Set of Acrylic</td>
+                    <td>$20.00 & up</td>
+                </tr>
+                <tr>
+                    <td>Fill of Acrylic</td>
+                    <td>$12.00 & up</td>
+                </tr>
+                <tr>
+                    <td>Full Set of Pink & White</td>
+                    <td>$28.00 & up</td>
+                </tr>
+                <tr>
+                    <td>Fill of Pink & White</td>
+                    <td>$22.00 & up</td>
+                </tr>
+                <tr>
+                    <td>Full Set of Silk Wrap</td>
+                    <td>$30.00</td>
+                </tr>
+                <tr>
+                    <td>Fill of Silk Wrap</td>
+                    <td>$20.00</td>
+                </tr>
+                <tr>
+                    <td>Full Set of Gel</td>
+                    <td>$28.00</td>
+                </tr>
+                <tr>
+                    <td>Fill of Gel</td>
+                    <td>$18.00</td>
+                </tr>
+                <tr>
+                    <td>Coating (Acrylic)</td>
+                    <td>$17.00</td>
+                </tr>
+                <tr>
+                    <td>Cut Down</td>
+                    <td>$2.00 EXTRA</td>
+                </tr>
+                <tr>
+                    <td>Nail Take Off</td>
+                    <td>$8.00</td>
+                </tr>
+                <tr>
+                    <td>Airbrush Design</td>
+                    <td>$5.00 & up</td>
+                </tr>
+                </tbody>
+            </table>
+            <br/>
+            <br/>
+            </div>
+        
         </div>
     )
 }
